5c41d5bc07988fe5d70db2e4dc87afefa62f4eb4
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-11-07 13:48:50 +0530

    Done

--- src/Components/Leave/Leave.jsx
+++ src/Components/Leave/Leave.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import './Leave.module.css';
-
+import leave from './Leave.module.css';
+import Header from '../Header/Header';
+import { FaDownload } from 'react-icons/fa';
 const Leave = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div>Leave</div>
+    <div>
+      <Header />
+      <div className={leave.leaveNavbar}>
+        <button className={leave.applyButton}>Apply</button>
+        <button className={leave.downloadButton}>
+          <FaDownload />
+        </button>
+        <span className={leave.year}>{currentYear}</span>
+      </div>
+    </div>
   )
 }
 
