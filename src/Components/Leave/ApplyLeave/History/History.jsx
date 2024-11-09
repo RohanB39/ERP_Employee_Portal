655b1d807fc64b7d1c3e04e619e87827d98940b5
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-11-09 07:50:02 +0530

    Done

---
+++ src/Components/Leave/ApplyLeave/History/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const History = () => {
+  return (
+    <div>
+      <h2>Leave History</h2>
+      <p>List of past leave requests will appear here.</p>
+    </div>
+  );
+};
+
+export default History;
